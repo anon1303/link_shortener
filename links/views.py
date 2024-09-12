979e07d9817d072ba591d49f6e12cff964299abf
anon1303 <jomari.daison@gmail.com>
2024-09-12 09:55:14 +0800

    added html to view links

--- links/views.py
+++ links/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Link
+
 
 # Create your views here.
+def index(request):
+    links = Link.objects.all()
+    content = {
+        'links':links
+    }
+    return render(request, 'links/index.html', content)
+
+# oursite.com/google -> www.google.com
+def root_link(request, link_slug):
+    link = get_object_or_404(Link, slug = link_slug)
+    # increment the counter to the link
+    link.click() #increment the clicked fields
